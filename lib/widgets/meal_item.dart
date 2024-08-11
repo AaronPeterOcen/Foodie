1a7a8eb2752bc97fccfa0be3f6bc1c7fc213f14b
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-08-11 06:00:08 +0300

    using the stack widget to stack the meal items

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:foodie/models/meal.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
   const MealItem({
@@ -12,10 +13,56 @@ class MealItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      margin: const EdgeInsets.all(8),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      clipBehavior: Clip.hardEdge,
+      elevation: 5,
       child: InkWell(
         onTap: () {},
         // Stack positions multiple widgets above each other
-        child: Stack(),
+        child: Stack(
+          children: [
+            FadeInImage(
+              placeholder: MemoryImage(kTransparentImage),
+              image: NetworkImage(meal.imageUrl),
+              fit: BoxFit.cover,
+              height: 150,
+              width: double.infinity,
+            ),
+            Positioned(
+              right: 0,
+              bottom: 0,
+              left: 0,
+              child: Container(
+                color: const Color.fromARGB(126, 1, 23, 61),
+                padding:
+                    const EdgeInsets.symmetric(vertical: 5, horizontal: 39),
+                child: Column(
+                  children: [
+                    Text(
+                      meal.title,
+                      maxLines: 2,
+                      textAlign: TextAlign.center,
+                      softWrap: true,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(203, 237, 237, 237),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      children: [],
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
