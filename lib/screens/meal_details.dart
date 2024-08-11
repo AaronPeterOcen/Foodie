b0dd29cb38ef3fa4a213742ebbadbcd6c8a9234f
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-08-11 12:53:38 +0300

    updates

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -16,11 +16,62 @@ class MealDetails extends StatelessWidget {
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Image.network(
-        meal.imageUrl,
-        fit: BoxFit.cover,
-        width: double.infinity,
-        height: 300,
+      body: SingleChildScrollView(
+        //for scrollability and fit items
+        child: Column(
+          // mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.network(
+              meal.imageUrl,
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: 300,
+            ),
+            const SizedBox(height: 14),
+            Text(
+              'Ingredients',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onSurfaceVariant,
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+            const SizedBox(height: 12),
+            // ignore: unused_local_variable
+            for (final ingredients in meal.ingredients)
+              Text(
+                ingredients,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color:
+                          Theme.of(context).colorScheme.onTertiaryFixedVariant,
+                    ),
+              ),
+            const SizedBox(height: 24),
+            Text(
+              'Steps',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onSurfaceVariant,
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+            const SizedBox(height: 12),
+            // ignore: unused_local_variable
+            for (final steps in meal.steps)
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 14),
+                child: Text(
+                  textAlign: TextAlign.center,
+                  '> $steps',
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                        color: Theme.of(context)
+                            .colorScheme
+                            .onTertiaryFixedVariant,
+                        fontWeight: FontWeight.normal,
+                      ),
+                ),
+              )
+          ],
+        ),
       ),
     );
   }
