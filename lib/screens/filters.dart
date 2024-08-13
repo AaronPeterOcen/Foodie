9288f2f57e08b19ed09a896d2b53b611633108ba
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-08-13 05:38:05 +0300

    adding more meal filter options

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -9,6 +9,9 @@ class FiltersScreen extends StatefulWidget {
 
 class _FiltersScreenState extends State<FiltersScreen> {
   var _isGlutenFree = false;
+  var _isLactoseFree = false;
+  var _isVegetarian = false;
+  var _isVegan = false;
 
   @override
   Widget build(BuildContext context) {
@@ -44,6 +47,78 @@ class _FiltersScreenState extends State<FiltersScreen> {
             ),
             subtitle: Text(
               'Only Gluten-free meals included.',
+              style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onSurfaceVariant,
+                  ),
+            ),
+            activeColor: Theme.of(context).colorScheme.primaryContainer,
+            inactiveTrackColor: Theme.of(context).colorScheme.primaryContainer,
+            contentPadding: const EdgeInsets.only(left: 28, right: 18),
+          ),
+          SwitchListTile(
+            // hoverColor: Colors.blue,
+            value: _isLactoseFree,
+            onChanged: (isChanged) {
+              setState(() {
+                _isLactoseFree = isChanged;
+              });
+            },
+            title: Text(
+              'Lactose-free',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onSurfaceVariant,
+                  ),
+            ),
+            subtitle: Text(
+              'Only Lactose-free meals included.',
+              style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onSurfaceVariant,
+                  ),
+            ),
+            activeColor: Theme.of(context).colorScheme.primaryContainer,
+            inactiveTrackColor: Theme.of(context).colorScheme.primaryContainer,
+            contentPadding: const EdgeInsets.only(left: 28, right: 18),
+          ),
+          SwitchListTile(
+            // hoverColor: Colors.blue,
+            value: _isVegan,
+            onChanged: (isChanged) {
+              setState(() {
+                _isVegan = isChanged;
+              });
+            },
+            title: Text(
+              'Vegan',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onSurfaceVariant,
+                  ),
+            ),
+            subtitle: Text(
+              'Only Vegan meals included.',
+              style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onSurfaceVariant,
+                  ),
+            ),
+            activeColor: Theme.of(context).colorScheme.primaryContainer,
+            inactiveTrackColor: Theme.of(context).colorScheme.primaryContainer,
+            contentPadding: const EdgeInsets.only(left: 28, right: 18),
+          ),
+          SwitchListTile(
+            // hoverColor: Colors.blue,
+            value: _isVegetarian,
+            onChanged: (isChanged) {
+              setState(() {
+                _isVegetarian = isChanged;
+              });
+            },
+            title: Text(
+              'Vegetarian',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onSurfaceVariant,
+                  ),
+            ),
+            subtitle: Text(
+              'Only vegetarian meals included.',
               style: Theme.of(context).textTheme.labelMedium!.copyWith(
                     color: Theme.of(context).colorScheme.onSurfaceVariant,
                   ),
