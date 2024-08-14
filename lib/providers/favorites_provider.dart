061fe2d6f0fc4c85334b2c7bbba08d127a0af9de
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-08-14 13:10:03 +0300

    using a more advanced provider with state notifier

---
+++ lib/providers/favorites_provider.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:foodie/models/meal.dart';
+
+// A custom StateNotifier class that manages the list of favorite meals.
+class FavoritesProviderNotifier extends StateNotifier<List<Meal>> {
+  // The constructor initializes the state with an empty list of meals.
+  FavoritesProviderNotifier() : super([]);
+
+  // This method toggles the favorite status of a given meal.
+  void toggleFavoritesMealState(Meal meal) {
+    // Check if the meal is already in the list of favorites.
+    final mealIsFavorite = state.contains(meal);
+
+    if (mealIsFavorite) {
+      // If the meal is already a favorite, remove it from the list.
+      state = state.where((m) => m.id != meal.id).toList();
+    } else {
+      // If the meal is not a favorite, add it to the list.
+      state = [...state, meal];
+    }
+  }
+}
+
+// Defining a StateNotifierProvider that manages a list of favorite meals.
+// This provider allows widgets to interact with the FavoritesProviderNotifier.
+final favoritesProvider =
+    StateNotifierProvider<FavoritesProviderNotifier, List<Meal>>((ref) {
+  // Return an instance of FavoritesProviderNotifier to handle the state.
+  return FavoritesProviderNotifier();
+});
--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:foodie/data/dummy_data.dart';
+// import 'package:foodie/data/dummy_data.dart';
 import 'package:foodie/models/meal.dart';
 import 'package:foodie/providers/meals_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
